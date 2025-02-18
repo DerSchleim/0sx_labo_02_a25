bba97dbc1a964577b3b2f3cfbab71c3ae32501aa
Madison_Ayotte <madisonayotte@outlook.fr>
2025-02-18 13:41:44 -0500

    Modification Labo_02

--- labo_02/labo_02.ino
+++ labo_02/labo_02.ino
@@ -1,9 +1,8 @@
 unsigned long currentTime = 0;
-int potentiometerPin = A1; 
+int potentiometerPin = A0; 
 const int ledPins[] = {8, 9, 10, 11};
 const int buttonPin = 2;
-int buttonState = 0;
-int potentiometerValue = 0; 
+int potentiometerValue = 0;
 int mappedIndex = 0;
 unsigned long serialPrevious = 0;
 int serialDelay = 500;
@@ -18,190 +17,80 @@ void setup() {
   Serial.println("Setup completed");
 }
 
-void barre_progress(){
-  potentiometerValue = analogRead(potentiometerPin);
-  mappedIndex = map (potentiometerValue, 0, 1023, 0, 20);
-
-  for (int i = 0; i < 20; i++){
-    if(mappedIndex <= 5){
-        switch(mappedIndex){
-          case 0:
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[....................] 0%");  
-          } 
-          break;
-          case 1 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[#...................] 5%");  
-          } 
-          break;
-          case 2 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[##..................] 10%"); 
-          } 
-          break;
-          case 3 :          
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[###.................] 15%");
-          } 
-          break;
-          case 4 :
-           if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[####................] 20%");
-          } 
-          break;
-          case 5 :
-           if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[#####...............] 25%");
-          } 
-          break;
+void barre_progress(int potentiometerValue,int mappedIndex){
+  for(int i = 0; i < 4; i++){
+    switch(mappedIndex){
+      case 0 :
+        if (currentTime - serialPrevious >= serialDelay) {
+          serialPrevious = currentTime;
+          Serial.println("[#####...............] 25%");
         }
-    }else if(mappedIndex <= 10){
-        switch(mappedIndex){
-          case 6 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[######..............] 30%");
-          } 
-          break;
-          case 7 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[#######.............] 35%");
-          } 
-          break;
-          case 8 : 
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[########............] 40%");
-          } 
-          break;
-          case 9 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[#########...........] 45%");
-          }
-          break;
-          case 10 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[##########..........] 50%");
-          }
-          break;
+      break;
+      case 1 :
+        if (currentTime - serialPrevious >= serialDelay) {
+          serialPrevious = currentTime;
+          Serial.println("[##########..........] 50%");
         }
-    }else if(mappedIndex <=15){
-        switch(mappedIndex){
-          case 11 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[###########.........] 55%");
-          }
-          break;
-          case 12 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[############........] 60%");
-          }
-          break;
-          case 13 :        
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[#############.......] 65%");
-          }
-          break;
-          case 14 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[##############......] 70%");
-          }
-          break;
-          case 15 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[###############.....] 75%");
-          }
-          break;
+      break;
+      case 2 :
+        if (currentTime - serialPrevious >= serialDelay) {
+          serialPrevious = currentTime;
+          Serial.println("[###############.....] 75%");
         }
-    }else {
-        switch(mappedIndex){
-          case 16 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[################....] 80%");
-          }
-          break;
-          case 17 :          
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[#################...] 85%");
-          }
-          break;
-          case 18 :         
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[##################..] 90%");
-          }
-          break;
-          case 19 :          
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[###################.] 95%");
-          }
-          break;
-          case 20 :
-          if (currentTime - serialPrevious >= serialDelay) {
-            serialPrevious = currentTime;
-            Serial.println("[####################] 100%");
-          }
-          break;
-      }
+      break;
+      case 3 :
+        if (currentTime - serialPrevious >= serialDelay) {
+          serialPrevious = currentTime;
+          Serial.println("[####################] 100%");
+        }
+      break;
     }
   }
-  
 }
 
-void LEDs(){
-  potentiometerValue = analogRead(potentiometerPin);
-  mappedIndex = map (potentiometerValue, 0, 1023, 0, 20); 
-  
-  if(mappedIndex < 5){
+
+void LEDs(int mappedIndex,int potentiometerValue){
+  switch(mappedIndex){
+    case 0:
       digitalWrite(ledPins[0], HIGH);
       digitalWrite(ledPins[1], LOW);
       digitalWrite(ledPins[2], LOW);
-      digitalWrite(ledPins[3], LOW);
-  }else if(mappedIndex < 10){
+      digitalWrite(ledPins[3], LOW);    
+    break;
+    case 1:   
       digitalWrite(ledPins[0], HIGH);
       digitalWrite(ledPins[1], HIGH);
       digitalWrite(ledPins[2], LOW);
-      digitalWrite(ledPins[3], LOW);
-  }else if(mappedIndex < 15){
+      digitalWrite(ledPins[3], LOW);     
+    break;
+    case 2:      
       digitalWrite(ledPins[0], HIGH);
       digitalWrite(ledPins[1], HIGH);
       digitalWrite(ledPins[2], HIGH);
-      digitalWrite(ledPins[3], LOW);
-  }else {
+      digitalWrite(ledPins[3], LOW);    
+    break;
+    case 3:   
       digitalWrite(ledPins[0], HIGH);
       digitalWrite(ledPins[1], HIGH);
       digitalWrite(ledPins[2], HIGH);
-      digitalWrite(ledPins[3], HIGH);
-  } 
-}
-
-void button(){
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == LOW) {
-   barre_progress();
+      digitalWrite(ledPins[3], HIGH); 
+    break; 
   }
 }
 
+void button(){
+  int buttonState = 0;
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == LOW){
+    barre_progress(potentiometerValue,mappedIndex);
+  }
+}
+
+
 void loop() {
+  potentiometerValue = analogRead(potentiometerPin);
+  mappedIndex = map (potentiometerValue, 0, 1023, 0, 3);
   currentTime = millis();
- button();
-  LEDs();
+  button();
+  LEDs(mappedIndex,potentiometerValue);
 }
